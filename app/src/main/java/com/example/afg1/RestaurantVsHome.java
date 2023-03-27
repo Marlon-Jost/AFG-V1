4590cf8463ba47ba3b955887006e157ae0d88899
Marlon-Jost <marlonfjost@gmail.com>
2023-03-27 11:35:34 -0400

    Marlon's commit message 1

---
+++ app/src/main/java/com/example/afg1/RestaurantVsHome.java
@@ -0,0 +1,14 @@
+package com.example.afg1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantVsHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_vs_home);
+    }
+}
