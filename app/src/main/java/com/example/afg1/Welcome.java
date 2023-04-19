9d57be5d9567a268d56d14141aac1fac99182299
purpleapplebird <annewu109@gmail.com>
2023-04-19 14:43:41 -0400

    started data storage for max carbs

--- app/src/main/java/com/example/afg1/Welcome.java
+++ app/src/main/java/com/example/afg1/Welcome.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.InputStream;
 
 public class Welcome extends AppCompatActivity {
     @Override
@@ -15,7 +19,16 @@ public class Welcome extends AppCompatActivity {
 
     //after start screen, go to home
     public void performHomePage(View v) {
+        EditText maxCarbs = findViewById(R.id.inputMaxCarbs);
+        int numOfMaxCarbs = Integer.parseInt(maxCarbs.getText().toString());
+
+        Button button = findViewById(R.id.button);
+
+        Meal meal= new Meal();
+        meal.setTotalCarbs(numOfMaxCarbs);
         Intent intent = new Intent(this, HomePage.class);
         startActivity(intent);
     }
+
+
 }
