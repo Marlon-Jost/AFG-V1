b204de5a328ca9680f194016a672922d359f4ab0
Marlon <marlonfjost@gmail.com>
2023-04-23 20:10:48 -0400

    I added the capability to search through the database in the RestaurantChoice class based on what the user types in. Right now it searches for he name of the order but this should be restaurant name when we add that to the database. Also, the filtered data still needs to go in some kind of list that can be passed to other classes.

--- app/src/main/java/com/example/afg1/RestaurantChoice.java
+++ app/src/main/java/com/example/afg1/RestaurantChoice.java
@@ -1,27 +1,48 @@
 package com.example.afg1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class RestaurantChoice extends AppCompatActivity {
+
+    private FirebaseDatabase data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_choice);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-            android.R.layout.simple_dropdown_item_1line, RESTAURANTS);
-        AutoCompleteTextView textView = (AutoCompleteTextView)
-                findViewById(R.id.restaurantDropDown);
-        textView.setAdapter(adapter);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//            android.R.layout.simple_dropdown_item_1line, RESTAURANTS);
+//        AutoCompleteTextView textView = (AutoCompleteTextView)
+//                findViewById(R.id.restaurantDropDown);
+//        textView.setAdapter(adapter);
+
+
     }
 
-    private static final String[] RESTAURANTS = new String[] {
+    public void setDatabase(FirebaseDatabase database) {
+        data = database;
+    }
+
+    private static final String[] RESTAURANTS = new String[]{
             "panera", "starbucks", "taco bell"
     };
 
@@ -42,5 +63,81 @@ public class RestaurantChoice extends AppCompatActivity {
     public void performHomePage(View v) {
         Intent intent = new Intent(this, HomePage.class);
         startActivity(intent);
+    }
+
+    public void performSearch(View v) {
+        //fetches the text entered in the text view the first time
+        EditText editText = (EditText) findViewById(R.id.searchBarRestaurant);
+        String text = editText.getText().toString();
+        Log.d("RestaurantChoice", "Text #1 is: " + text);
+
+        //continually updates the value of "text" as the user edits the input text
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Do nothing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Call your method here with the text entered by the user
+                String text = s.toString();
+                //passes "text" to the search method below
+                search(text);
+                Log.d("RestaurantChoice", "Text #2 is: " + text);
+            }
+        });
+    }
+
+    //should search through our database by sorting the children within a snapshot of our class according the the specified query and looping through the remaining children
+    public void search(String name) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Orders");
+
+        Query query = myRef.orderByChild("orderName").startAt(name).endAt(name + "\uf8ff");
+
+//        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                int size = (int) dataSnapshot.getChildrenCount();
+//                Log.d("RestaurantChoice","Size pre filtered: " + Integer.toString(size));
+//                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+//                    Log.d("RestaurantChoice", String.valueOf(childSnapshot));
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                // Handle errors here
+//            }
+//        });
+
+
+        // For Firebase Realtime Database
+        query.addListenerForSingleValueEvent(new ValueEventListener() { //filters the snapshot
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                int size = (int) snapshot.getChildrenCount();
+                Log.d("RestaurantChoice","Size post filtered: " + Integer.toString(size));
+                if (snapshot.exists()) {
+                    for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                        Log.d("RestaurantChoice", "Key is: " + childSnapshot.getKey() + " value is: " + childSnapshot.getValue());
+                    }
+                } else {
+                    Log.d("RestaurantChoice", "No data available");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                System.err.println(error.getMessage());
+            }
+        });
+
     }
 }
